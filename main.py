3dfd1560e081eaaf2f2339ed130e49e2e02146d5
GKingA <kinga.andrea.gemes@gmail.com>
2022-10-28 16:42:39 +0200

    Argparser added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 from typing import List, Dict
 from regexes import regex_dict
 import warnings
+import os
+import argparse
 
 
 def print_warning(text: str) -> Dict[str, List[str]]:
@@ -15,3 +17,29 @@ def print_warning(text: str) -> Dict[str, List[str]]:
                                   f"\"{group.strip()}\" could be a(n) {reg_name}!")
     return regs
 
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--file", "-f", nargs="+",
+                           help="Define specific files for the check")
+    argparser.add_argument("--path", "-p", nargs="+",
+                           help="Define a directory or multiple directories in which all files will be checked")
+    argparser.add_argument("--string", "-s", help="Pass a string directly to the method")
+    args = argparser.parse_args()
+
+    if args.file is None and args.path is None and args.string is None:
+        warnings.warn("No file or string given for the check")
+    if args.file is not None:
+        for file in args.file:
+            print(file)
+            with open(file) as check_file:
+                print_warning(check_file.read())
+    if args.path is not None:
+        for path in args.path:
+            for file in os.listdir(path):
+                file_path = os.path.join(path, file)
+                print(file_path)
+                with open(file_path) as check_file:
+                    print_warning(check_file.read())
+    if args.string is not None:
+        print_warning(args.string)
