523ef9049504e658ff95726c187ca96a622557d8
Gabor Recski <gabor.recski@tuwien.ac.at>
2022-11-04 08:55:14 +0100

    tools to evaluate performance, some refactoring, updated README

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from collections import defaultdict
-from typing import List, Dict
-from regexes import regex_dict
-import warnings
-import os
 import argparse
+import os
+import sys
+import time
+import warnings
+from collections import Counter, defaultdict
+from datetime import timedelta
+from io import StringIO
+from typing import List, Dict
+
+from regexes import regex_dict
+from utils import sizeof_fmt
 
 
 def print_warning(text: str) -> Dict[str, List[str]]:
@@ -13,33 +19,79 @@ def print_warning(text: str) -> Dict[str, List[str]]:
             if match is not None:
                 if group := match.group():
                     regs[reg_name].append(group.strip())
-                    warnings.warn(f"There might be sensitive information in the text! "
-                                  f"\"{group.strip()}\" could be a(n) {reg_name}!")
+                    warnings.warn(
+                        f"There might be sensitive information in the text! "
+                        f'"{group.strip()}" could be a(n) {reg_name}!'
+                    )
     return regs
 
 
-if __name__ == "__main__":
+def check_buffers(buffers):
+    stats = Counter()
+    total_length = 0
+    total_size = 0
+    start = time.time()
+    for buffer in buffers:
+        text = buffer.read()
+        total_size += sys.getsizeof(text)
+        total_length += len(text)
+        regs = print_warning(text)
+        for reg_name, match_list in regs.items():
+            stats[reg_name] += len(match_list)
+
+    end = time.time()
+    total = sum(stats.values())
+    elapsed = timedelta(seconds=end - start)
+    print(
+        f"done, processed {sizeof_fmt(total_size)} text in {elapsed}, found {total} matches"
+    )
+    print("most common categories:", stats.most_common())
+
+
+def get_args():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--file", "-f", nargs="+",
-                           help="Define specific files for the check")
-    argparser.add_argument("--path", "-p", nargs="+",
-                           help="Define a directory or multiple directories in which all files will be checked")
-    argparser.add_argument("--string", "-s", help="Pass a string directly to the method")
-    args = argparser.parse_args()
+    argparser.add_argument(
+        "--file", "-f", nargs="+", help="Define specific files for the check"
+    )
+    argparser.add_argument(
+        "--path",
+        "-p",
+        nargs="+",
+        help="Define a directory or multiple directories in which all files will be checked",
+    )
+    argparser.add_argument(
+        "--string", "-s", help="Pass a string directly to the method"
+    )
+    argparser.add_argument("-q", "--quiet", default=False, action="store_true")
+    return argparser.parse_args()
+
+
+def main():
+    args = get_args()
 
     if args.file is None and args.path is None and args.string is None:
         warnings.warn("No file or string given for the check")
+
+    if args.quiet:
+        warnings.simplefilter("ignore")
+
+    buffers = []
+
     if args.file is not None:
-        for file in args.file:
-            print(file)
-            with open(file) as check_file:
-                print_warning(check_file.read())
+        buffers += [open(file) for file in args.file]
+
     if args.path is not None:
-        for path in args.path:
-            for file in os.listdir(path):
-                file_path = os.path.join(path, file)
-                print(file_path)
-                with open(file_path) as check_file:
-                    print_warning(check_file.read())
+        buffers += [
+            open(os.path.join(path, file))
+            for path in args.path
+            for file in os.listdir(path)
+        ]
+
     if args.string is not None:
-        print_warning(args.string)
+        buffers.append(StringIO(args.string))
+
+    check_buffers(buffers)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,8 @@
+
+def sizeof_fmt(num, suffix="B"):
+    """from https://stackoverflow.com/a/1094933/2753770"""
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
